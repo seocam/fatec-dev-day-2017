8ef383e14d9ad1ab9f07e8771e206d83e97d945e
Sergio Oliveira <seocam@seocam.com>
2017-11-10 23:54:41 -0200

    Status Code 404

--- http-server.py
+++ http-server.py
@@ -3,7 +3,7 @@ from socketserver import BaseRequestHandler, TCPServer
 
 
 def index():
-    return b"<h1>Ola Mundo!</h1>"
+    return "<h1>Ola Mundo!</h1>"
 
 
 class HTTPRequestHander(BaseRequestHandler):
@@ -14,13 +14,17 @@ class HTTPRequestHander(BaseRequestHandler):
         request_lines = self.data.split('\r\n')
         first_line = request_lines[0]
         verb, path, version = first_line.split(' ')
+        status = '200 OK'
 
         if path == '/':
             response = index()
+        else:
+            status = '404 Not Found'
+            response = '<h1>Pagina nao encontrada!</h1>'
 
-        self.request.sendall(b"HTTP/1.1 200 OK\r\n")
+        self.request.sendall("HTTP/1.1 {}\r\n".format(status).encode('utf-8'))
         self.request.sendall(b"Content-Type: text/html\r\n\r\n")
-        self.request.sendall(response)
+        self.request.sendall(response.encode('utf-8'))
 
 
 if __name__ == "__main__":
