e3310f4caf19c845e16796fa00964f26bdd1dc2a
Sergio Oliveira <seocam@seocam.com>
2017-11-10 23:11:44 -0200

    Header de tipo de conteúdo adicionado

--- http-server.py
+++ http-server.py
@@ -7,6 +7,8 @@ class HTTPRequestHander(BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print(self.data.decode('utf-8'))
 
+        self.request.sendall(b"HTTP/1.1 200 OK\r\n")
+        self.request.sendall(b"Content-Type: text/html\r\n\r\n")
         self.request.sendall(b"<h1>Ola Mundo!</h1>")
 
 
